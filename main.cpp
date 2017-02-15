3a6604b88276e7b7de50db23273684c9e8ad69db
rbulteau <romain.bulteau@insa-lyon.fr>
2017-02-15 17:27:02 +0100

    Added main.cpp

---
+++ main.cpp
@@ -0,0 +1,32 @@
+//==============================
+//    INCLUDES
+//==============================
+
+#include <cstdio>
+#include <cstdlib>
+#include <fstream>
+#include <vector>
+#include <list>
+
+
+#include "Environment.h"
+#include "Spot.h"
+#include "Cell.h"
+#include "CellA.h"
+#include "CellB.h"
+
+//==============================
+//    FUNCTION DECLARATION
+//==============================
+
+
+//==============================
+//    MAIN
+//==============================
+
+int main(int argc, char const *argv[])
+{
+  
+  return 0;
+}
+
